b4a89734c74d1b7e3f8227cfc64fdf3468262aa7
jasontung2214 <jasontung2214@gmail.com>
2017-12-15 00:38:08 +0800

    1. 新增gameStateController和gameStateData

---
+++ Assets/Scripts/Controller/GameStateController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour {
+    private EnemyController enemyController;
+    private AudioController audioController;
+    private GameUIController gameUIController;
+    private PlayerData playerData;
+    private GameStateData gameStateData;
+    
+    private void Awake()
+    {
+        Input.multiTouchEnabled = true;
+        playerData = GameFacade.GetInstance().playerData;
+        gameStateData = GameFacade.GetInstance().gameStateData;
+        audioController = GameFacade.GetInstance().AudioController;
+        enemyController = GameFacade.GetInstance().EnemyController;
+        gameUIController = GameFacade.GetInstance().GameUIController;
+    }
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(PlayPhase());
+            yield return StartCoroutine(EndPhase());
+        }
+    }
+
+    IEnumerator PlayPhase()
+    {
+        yield return StartCoroutine(enemyController.Execute());
+    }
+
+    IEnumerator EndPhase()
+    {
+        audioController.Stop();
+        if (gameStateData.isFail)
+        {
+            yield return StartCoroutine(gameUIController.stageFailEffect.Show());
+        }
+        else
+        {
+            yield return StartCoroutine(gameUIController.stageClearEffect.Show());
+            gameStateData.NextStage();
+        }
+        gameStateData.isFail = false;
+    }
+}
---
+++ Assets/Scripts/Model/GameStateData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+[Serializable]
+public class GameStateData
+{
+    public bool isFail;
+    private PlayerData PlayerData
+    {
+        get
+        {
+            return GameFacade.GetInstance().playerData;
+        }
+    }
+    private StageData[] StageDatas
+    {
+        get
+        {
+            return GameFacade.GetInstance().stageDatas;
+        }
+    }
+    public StageData CurStageData
+    {
+        get
+        {
+            return StageDatas[PlayerData.stageIndex];
+        }
+    }
+
+    public void NextStage()
+    {
+        PlayerData.stageIndex = Mathf.Min(PlayerData.stageIndex + 1, StageDatas.Length - 1);
+    }
+}
