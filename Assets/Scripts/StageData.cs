190205bce3bbe049c5a3c084fded4c6e0023d091
jasontung2214 <jasontung2214@gmail.com>
2017-12-14 15:44:53 +0800

    delete all scripts

--- Assets/Scripts/StageData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-[CreateAssetMenu]
-public class StageData : ScriptableObject {
-    public EnemyData[] enemys;
-}
