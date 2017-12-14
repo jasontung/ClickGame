9c14425efd87d373e6b3a5dca16bec22f2a483d6
4419-TEACHER\user <jasontung>
2017-12-14 21:53:26 +0800

    1. 新增enemyBehavior MeshFader

---
+++ Assets/Scripts/View/EnemyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
+public class EnemyBehavior : MonoBehaviour {
+    private MeshFader meshFader;
+    private Animator animator;
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());
+    }
+    
+    [ContextMenu("Test Damage")]
+    public void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+            DoDamage(10);
+    }
+    public void DoDamage(int attack)
+    {
+        animator.SetTrigger("hurt");
+        audioSource.Play();
+    }
+    public IEnumerator Execute()
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/View/MeshFader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFader : MonoBehaviour {
+    public Renderer[] fadeRenderers;
+    public float speed = 1; 
+
+	public IEnumerator FadeIn()
+    {
+        StopCoroutine(FadeOut());
+        float alpha = 0;
+        ChangeAlpha(alpha);
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime * speed;
+            alpha = Mathf.Min(alpha, 1);
+            ChangeAlpha(alpha);
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOut()
+    {
+        StopCoroutine(FadeIn());
+        float alpha = 1;
+        ChangeAlpha(alpha);
+        while (alpha > 0)
+        {
+            alpha -= Time.deltaTime * speed;
+            alpha = Mathf.Max(alpha, 0);
+            ChangeAlpha(alpha);
+            yield return null;
+        }
+    }
+
+    private void ChangeAlpha(float alpha)
+    {
+        for (int i = 0; i < fadeRenderers.Length; i++)
+        {
+            Color color = fadeRenderers[i].material.color;
+            color.a = alpha;
+            fadeRenderers[i].material.color = color;
+        }
+    }
+}
