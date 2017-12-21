0859d71412f62c8b5e465b6903b8504f2abebc7c
4419-TEACHER\user <jasontung>
2017-12-21 21:15:51 +0800

    1.新增behavior和component資料夾 2.完成healthComponent血條控制 3. 完成敵人基本生命流程 4. 完成UI Camera

---
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(HealthComponent))]
+public class EnemyBehavior : MonoBehaviour {
+    private MeshFader meshFader;
+    private Animator animator;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+    [SerializeField]
+    private AudioClip hurtClip;
+    [SerializeField]
+    private AudioClip deadClip;
+    public bool IsDead
+    {
+        get
+        {
+            return healthComponent.IsOver;
+        }
+    }
+
+    #region Public Method
+    /// <summary>
+    /// 請呼叫我 開始敵人生命邏輯
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Execute()
+    {
+        healthComponent.Init(100);
+        while(IsDead == false)
+        {
+            yield return null;
+        }
+        animator.SetTrigger("die");
+        audioSource.clip = deadClip;
+        audioSource.Play();
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        yield return StartCoroutine(meshFader.FadeOut());
+    }
+    public void DoDamage(int attack)
+    {
+        healthComponent.Hurt(attack);
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+    }
+    #endregion
+
+    #region Private Method
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());
+    }
+
+    [ContextMenu("Test Execute")]
+    private void TestExecute()
+    {
+        StartCoroutine(Execute());
+    }
+    private void Update()
+    {
+        if (IsDead)
+            return;
+        if(Input.GetButtonDown("Fire1"))
+            DoDamage(50);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/View/Component/HealthComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthComponent : MonoBehaviour {
+    public Slider healthSlider;
+    private int currentHealth;
+    public float smooth = 5f;
+    public bool IsOver
+    {
+        get
+        {
+            return currentHealth <= healthSlider.minValue;
+        }
+    }
+
+    public void Init(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = healthSlider.maxValue;
+        currentHealth = (int)healthSlider.maxValue;
+    }
+
+    public void Hurt(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = (int)Mathf.Max(healthSlider.minValue, currentHealth);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, Time.deltaTime * smooth);
+	}
+}
